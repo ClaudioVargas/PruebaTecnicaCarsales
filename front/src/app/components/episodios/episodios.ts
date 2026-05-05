26aa31243af7643b84a271c1d763877205703fe2
ClaudioVargas <claudiovargaslillo@gmail.com>
2026-05-05 01:33:57 -0400

    se registrar correctamente HttpClient, se Implementa búsqueda por nombre, filtro por temporada y paginación se Separar visualmente los estados de carga, error y sin resultados

--- front/src/app/components/episodios/episodios.ts
+++ front/src/app/components/episodios/episodios.ts
@@ -1,23 +1,49 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, ViewChild } from '@angular/core';
 import { RickandmortyService } from '../../services/rickandmortyService';
 import { EpisodioModel } from '../../models/episodio.model';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialog } from '@angular/material/dialog';
 import { Dialog } from '../dialog/dialog';
+import { MatPaginator, MatPaginatorModule, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSort } from '@angular/material/sort';
+import { MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { FormControl } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-episodios',
   imports: [
-    MatProgressSpinnerModule],
+    CommonModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule],
   templateUrl: './episodios.html',
   styleUrl: './episodios.css',
 })
 export class Episodios implements OnInit {
 
 
-  episodios = signal<EpisodioModel[]>([])
+  // dataSource!: MatTableDataSource<EpisodioModel>;
+  dataSource = new MatTableDataSource<EpisodioModel>([]);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  // @ViewChild(MatSort) sort!: MatSort;
 
   loading = signal<boolean>(false)
+
+  displayedColumns: string[] = ['id', 'name', 'episode', 'actions'];
+
+
+
+  toppings = new FormControl();
+  temporadas!: string[]
+
+  mensajeError = "";
 
   constructor(
     private rickandmortingService: RickandmortyService,
@@ -29,31 +55,24 @@ export class Episodios implements OnInit {
     this.loading.set(true)
     this.rickandmortingService.getEpisodios().subscribe({
       next: (data) => {
-        this.episodios.set(data.results)
-        // setTimeout(()=>{
+        data.results.forEach((el) => {
+          el.temporada = el.episode.substring(0, 3)
+        })
+        this.dataSource = new MatTableDataSource(data.results)
+        this.dataSource.paginator = this.paginator
+        this.obtenerTemporadas()
+        this.definirFiltrosTemporada()
         this.loading.set(false)
-        // },3000)
-
-        console.log("this.data", data)
-        console.log("this.episodios", this.episodios)
+        this.mensajeError = "No fue posible cargar datos"
+        console.log("this.dataSource", this.dataSource)
       },
       error: (err) => {
         console.error('Error al cargar datos', err)
+        this.mensajeError = "Tenemos problemas de conexion, contacte con el administrador"
         this.loading.set(false)
       }
     });
-
-
-
-
-    // this.rickandmortingService.getEpisodios().subscribe((response)=>{
-    //   console.log("response", response)
-    //   this.episodios = response.results
-    //   console.log("this.episodios", this.episodios)
-
-    // });
   }
-
 
   verDetalle(id: number) {
     console.log("id*********", id)
@@ -62,7 +81,7 @@ export class Episodios implements OnInit {
       // this.episodios = response.results
       console.log("****reponse verDetalle", response)
       this.openDialog(response)
-      
+
 
     });
   }
@@ -77,4 +96,116 @@ export class Episodios implements OnInit {
       console.log('The dialog was closed => result =>', result);
     });
   }
+
+
+  length = 50;
+  pageSize = 10;
+  pageIndex = 0;
+  pageSizeOptions = [5, 10, 25];
+
+  hidePageSize = false;
+  showPageSizeOptions = true;
+  showFirstLastButtons = true;
+  disabled = false;
+
+  pageEvent: PageEvent | undefined;
+
+  // handlePageEvent(e: PageEvent) {
+  //   this.pageEvent = e;
+  //   this.length = e.length;
+  //   this.pageSize = e.pageSize;
+  //   this.pageIndex = e.pageIndex;
+  // }
+
+  setPageSizeOptions(setPageSizeOptionsInput: string) {
+    if (setPageSizeOptionsInput) {
+      this.pageSizeOptions = setPageSizeOptionsInput.split(',').map(str => +str);
+    }
+  }
+
+
+  obtenerTemporadas() {
+    // Creamos un nuevo arreglo con las 3 primeras letras
+    this.temporadas = Array.from(
+      new Set(this.dataSource.data.map(item => item.episode.substring(0, 3)))
+    );
+    console.log("temporadas", this.temporadas);
+  }
+
+  // applyFilter(event: Event) {
+  //   console.log("*******************************")
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   console.log("filterValue", filterValue)
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
+  // onSelectionChange(event: MatSelectChange) {
+  //   if (event.value.length > 0) {
+  //     this.dataSource.filter = JSON.stringify(event.value);
+  //   } else {
+  //     this.dataSource.filter = '';
+  //     this.dataSource.paginator?.firstPage();
+  //   }
+
+  // }
+
+  onSelectionChange(event: MatSelectChange | Event) {
+    console.log("onSelectionChange", event, typeof event)
+    if (event instanceof KeyboardEvent) {
+      console.log("KeyboardEvent")
+      const filterValue = (event.target as HTMLInputElement).value;
+      console.log("filterValue", filterValue)
+      this.dataSource.filter = filterValue.trim().toLowerCase();
+
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
+    }
+    if (event instanceof MatSelectChange) {
+      console.log("MatSelectChange")
+
+      if (event.value.length > 0) {
+        this.dataSource.filter = JSON.stringify(event.value);
+      } else {
+        this.dataSource.filter = '';
+        this.dataSource.paginator?.firstPage();
+      }
+    }
+  }
+
+
+  definirFiltrosTemporada() {
+    // 2. Sobrescribir filterPredicate
+    this.dataSource.filterPredicate = (data: EpisodioModel, filter: string) => {
+      // Si no hay filtros, mostrar todo
+      console.log("typeof filter", typeof filter )
+      if (!filter) return true;
+      if(this.esJSONValido(filter)) {
+        const temporadasFiltro = JSON.parse(filter);
+        // Retorna true si el episodio coincide con alguna temporada seleccionada
+        if (temporadasFiltro) {
+          return temporadasFiltro.includes(data.temporada);
+        }
+      } 
+      else {
+        return data.id.toString() == filter || 
+           data.name.toLowerCase().includes(filter) || 
+           data.episode.toLowerCase().includes(filter);
+      }
+    };
+  }
+
+  esJSONValido(str: string) {
+    try {
+        JSON.parse(str);
+    } catch (e) {
+        return false; // No es JSON válido
+    }
+    return true; // Es JSON válido
 }
+}
+
+
--- front/src/app/models/episodio.model.ts
+++ front/src/app/models/episodio.model.ts
@@ -3,6 +3,7 @@ export interface EpisodioModel {
       "name": string,
       "air_date": string,
       "episode": string,
+      "temporada": string,
       "characters": string [],
       "url": string,
       "created": Date
